31e1835edb1ea5652e3fc70a610fad19bf719cd4
GachigiMwangi <106179224+Betty20000@users.noreply.github.com>
2022-10-24 11:43:18 +0000

    a code is changed

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const crypto = require('crypto');
-const alice = crypto.getDiffieHellman('modp5');
-const bob = crypto.getDiffieHellman('modp5');
+  function guess(){
+           
+            input=document.getElementById("enter");
+            if(input >=0){
+                alert("You got it");
+            }
 
-alice.generateKeys();
-bob.generateKeys();
-
-const alice_secret = alice.computeSecret(
-    bob.getPublicKey(), null, 'hex'
-);
-const bob_secret = bob.computeSecret(
-    alice.getPublicKey(), null, 'hex'
-);
-
-// alice_secret and bob_secret should be the same
-console.log(alice_secret == bob_secret);
+        }
+  function enter(){
+            
+            input=document.getElementById("enter");
+            
+                alert("You got it");
